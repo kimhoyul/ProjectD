9eb5a077241bb532bfaa62a187056202b9bf7f4a
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2023-12-01 17:52:32 +0900

    Thread 공부

--- ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
+++ ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
@@ -2,7 +2,32 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread() // Thread Entry Point
+{
+	cout << "Hello Thread" << endl;
+}
+
+void HelloThread_2(int32 num) // Thread Entry Point
+{
+	cout << num << endl;
+}
+
 int main()
 {
-    HelloWorld();
+	vector<std::thread> v; // Vector of Threads
+
+	for (int i = 0; i < 10; ++i)
+	{
+		v.push_back(std::thread(HelloThread_2, i)); // Create Thread
+	}
+
+	for (int i = 0; i < 10; ++i)
+	{
+		if (v[i].joinable()) // Check if Thread is Joinable
+			v[i].join(); // Wait for Thread
+	}
+
+	cout << "Hello Main" << endl;
 }
