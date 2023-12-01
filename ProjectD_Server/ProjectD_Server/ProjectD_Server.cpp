023eb328792d81ee48c71297ff7ca7c136ce4c8c
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2023-12-01 20:05:15 +0900

    데드락 공부(교착상태 와 그 해결)

--- ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
+++ ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
@@ -5,51 +5,33 @@
 #include <atomic>
 #include <mutex>
 
-vector<int32> v;
+#include "UserManager.h"
+#include "AccountManager.h"
 
-// mutex : mutual exclusion (상호 배제)
-mutex m;
-
-// RAII patten : Resource Acquisition Is Initialization (자원 획득은 초기화 과정에서)
-// 이 패턴의 핵심 아이디어는 객체의 생명주기를 통해 자원의 관리를 자동화하는 것
-// 즉, 객체가 생성될 때 자원을 획득(acquisition)하고, 객체가 소멸될 때 자원을 해제(release)
-// 이 패턴을 사용하면 자원을 획득한 후, 예외가 발생하더라도 자원을 해제할 수 있음
-
-template<typename T>
-class LockGuard
+void Func1()
 {
-public:
-	LockGuard(T& m)
+	for (int i = 0; i < 100; i++)
 	{
-		_mutex = &m;
-		_mutex->lock();
+		UserManager::Instance()->ProcessSave();
 	}
+}
 
-	~LockGuard()
-	{
-		_mutex->unlock();
-	}
-
-private:
-	T* _mutex;
-};
-
-void push()
+void Func2()
 {
-	for (int32 i = 0; i < 10'000; ++i)
+	for (int i = 0; i < 100; i++)
 	{
-		std::lock_guard<std::mutex> lockGuard(m);
-		v.push_back(i);
+		AccountManager::Instance()->ProcessLogin();
 	}
 }
 
 int main()
 {
-	std::thread t1(push);
-	std::thread t2(push);
+	std::thread t1(Func1);
+	std::thread t2(Func2);
 
 	t1.join();
 	t2.join();
 
-	cout << v.size() << endl;
+	cout << "jobs done" << endl;
+
 }
