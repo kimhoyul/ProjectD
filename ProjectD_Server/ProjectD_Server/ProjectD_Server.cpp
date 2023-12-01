b6e80ed55ab447de6e89daff5a8d490ae6bf7172
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2023-12-01 18:16:08 +0900

    Atomic 공부

--- ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
+++ ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
@@ -1,33 +1,40 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
-
 #include <thread>
+#include <atomic>
 
-void HelloThread() // Thread Entry Point
+// atomic : 원자적 연산을 보장해주는 클래스
+// 원자적 연산 : 연산이 완전히 끝날 때까지 다른 스레드가 접근하지 못하도록 보장하는 연산
+atomic<int32> sum = 0; // global variable
+
+void Add()
 {
-	cout << "Hello Thread" << endl;
+	for (int i = 0; i < 1'000'000; ++i)
+	{
+		sum.fetch_add(1); // sum = sum + 1
+	}
 }
 
-void HelloThread_2(int32 num) // Thread Entry Point
+void Sub()
 {
-	cout << num << endl;
+	for (int i = 0; i < 1'000'000; ++i)
+	{
+		sum.fetch_sub(1); // sum = sum - 1
+	}
 }
 
 int main()
 {
-	vector<std::thread> v; // Vector of Threads
+	Add();
+	Sub();
+	
+	cout << sum << endl;
 
-	for (int i = 0; i < 10; ++i)
-	{
-		v.push_back(std::thread(HelloThread_2, i)); // Create Thread
-	}
+	std::thread t1(Add);
+	std::thread t2(Sub);
+	t1.join();
+	t2.join();
 
-	for (int i = 0; i < 10; ++i)
-	{
-		if (v[i].joinable()) // Check if Thread is Joinable
-			v[i].join(); // Wait for Thread
-	}
-
-	cout << "Hello Main" << endl;
+	cout << sum << endl;
 }
