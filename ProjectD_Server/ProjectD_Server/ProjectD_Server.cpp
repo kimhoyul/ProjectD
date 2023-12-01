48272ab16bdee770b0670b0d02dc95bdde9f5155
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2023-12-01 21:06:57 +0900

    SpinLock 구현 (CAS)

--- ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
+++ ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
@@ -5,33 +5,67 @@
 #include <atomic>
 #include <mutex>
 
-#include "UserManager.h"
-#include "AccountManager.h"
-
-void Func1()
+class SpinLock
 {
-	for (int i = 0; i < 100; i++)
+public:
+	void lock()
 	{
-		UserManager::Instance()->ProcessSave();
+		// CAS (Compare And Swap)
+		// _locked.compare_exchange_strong (expected, desired)
+		// expected == _locked 이면 _locked = desired
+		// expected != _locked 이면 expected = _locked
+
+		// 의사 코드를 유심히 봐야함 
+		// expected != _locked 경우에만!!!! expected = _locked를 해줌
+
+		bool expected = false;
+		bool desired = true;
+
+		while (_locked.compare_exchange_strong(expected, desired) == false)
+		{
+			expected = false;
+		}
+	}
+
+	void unlock()
+	{
+		_locked.store(false);
+	}
+
+private:
+	atomic<bool> _locked = false;
+};
+
+int32 sum = 0;
+mutex m;
+
+SpinLock spinLock;
+
+void Add()
+{
+	for (int i = 0; i < 100'000; ++i)
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		sum++;
 	}
 }
 
-void Func2()
+void Sub()
 {
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 100'000; ++i)
 	{
-		AccountManager::Instance()->ProcessLogin();
+		lock_guard<SpinLock> guard(spinLock);
+		sum--;
 	}
 }
 
 int main()
 {
-	std::thread t1(Func1);
-	std::thread t2(Func2);
+	thread t1(Add);
+	thread t2(Sub);
 
 	t1.join();
 	t2.join();
 
-	cout << "jobs done" << endl;
-
+	cout << sum << endl;
 }
