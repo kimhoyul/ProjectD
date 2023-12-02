24966eb9a1f27cb4b68582357422499d0b79d061
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2023-12-02 20:37:38 +0900

    LockFreeStack 3

--- ProjectD_Server/ProjectD_Server/ConcureentStack.h
+++ ProjectD_Server/ProjectD_Server/ConcureentStack.h
@@ -43,165 +43,141 @@ private:
 	condition_variable _condVar;
 };
 
+//template<typename T>
+//class LockFreeStack // 간지나지만 효율 개똥망인 스택 : 어렵지만 그렇다고 락 효율이 좋은 것도 아님
+//{
+//	struct Node
+//	{
+//		Node(const T& value) : data(make_shared<T>(value)), next(nullptr)
+//		{
+//
+//		}
+//
+//		shared_ptr<T> data;
+//		shared_ptr<Node> next;
+//	};
+//
+//public:
+//	void Push(const T& value)
+//	{
+//		shared_ptr<Node> node = make_shared<Node>(value);
+//		node->next = std::atomic_load(&_head);
+//
+//		while (std::atomic_compare_exchange_weak(&_head, &node->next, node) == false)
+//			;
+//	}
+//
+//	shared_ptr<T> TryPop()
+//	{
+//		shared_ptr<Node> oldHead = std::atomic_load(&_head);
+//
+//		while (oldHead && std::atomic_compare_exchange_weak(&_head, &oldHead, oldHead->next) == false)
+//			;
+//
+//		if (oldHead == nullptr)
+//			return shared_ptr<T>();
+//
+//		return oldHead->data;
+//	}
+//
+//private:
+//	shared_ptr<Node> _head;
+//};
+
 template<typename T>
 class LockFreeStack // 간지나지만 효율 개똥망인 스택 : 어렵지만 그렇다고 락 효율이 좋은 것도 아님
 {
+	struct Node;
+
+	struct CountedNodePtr
+	{
+		int32 externalCount = 0;
+		Node* ptr = nullptr;
+	};
+
 	struct Node
 	{
-		Node(const T& value) : data(value), next(nullptr)
+		Node(const T& value) : data(make_shared<T>(value))
 		{
 
 		}
 
-		T data;
-		Node* next;
+		shared_ptr<T> data;
+		atomic<int32> internalCount = 0;
+		CountedNodePtr next;
 	};
 
 public:
-	// [value][ ][ ][ ][ ][ ][ ]
-	//  ^  
-	// [_head]
-	// 1) 새 노드를 만든다.
-	// 2) 새 노드의 next = _head
-	// 3) _head = 새 노드
 	void Push(const T& value)
 	{
-		Node* node = new Node(value); // 1) 새 노드를 만든다.
-		node->next = _head.load(); // 2) 새 노드의 next = _head
-
-
-		// 윗줄의 코드에서 node->next = _head.load() 변경 하였으나 
-		// 본 스레드가 [ 3) _head = 새 노드 ] 를 하기전 다른 스레드가 윗줄을 통해 _head를 변경할 수 있으므로
-		// CAS(Compare And Swap)을 사용하여 변경
-		/* if (_head == node->next)  // 현재 기준으로 _head와 node->next가 같으면
-		{
-			_head = node; // 3) _head = 새 노드 를 해주고
-			return true;
-		}
-		else // 현재 기준으로 _head와 node->next가 다르면
-		{
-			node->next = _head; // 2) 새 노드의 next = _head 를 다시 해주고
-			return false;
-		}*/
-		while (_head.compare_exchange_weak(node->next, node) == false); // 3) _head = 새 노드
+		CountedNodePtr node;
+		node.ptr = new Node(value);
+		node.externalCount = 1;
+		
+		// _head에 넣어주기
+		node.ptr->next = _head;
+		while (_head.compare_exchange_weak(node.ptr->next, node) == false)
+			;
 	}
 
-	// OUT<-[X] [ ][ ][ ][ ][ ][ ]
-	//           ^
-	//           [_head]
-	// 1) oldHead = _head
-	// 2) _head = _head->next
-	// 3) 기존 _head data 추출 / 반환
-	// 4) 기존 _head 삭제
-	bool TryPop(T& value)
+	shared_ptr<T> TryPop()
 	{
-		++_popcount; // popcount 증가
-
-		Node* oldHead = _head.load(); // 1) oldHead = _head
-		
-		// 윗줄의 코드에서 oldHead = _head.load() 하였으나 
-		// 본 스레드가 [ 2) _head = _head->next ] 를 하기전 다른 스레드가 윗줄을 통해 _head를 변경할 수 있으므로
-		// CAS(Compare And Swap)을 사용하여 변경
-		/*if (_head == oldHead) // 현재 기준으로 _head와 oldHead가 같으면 
+		CountedNodePtr oldHead = _head;
+		while (true)
 		{
-			_head = _head->next; // 2) _head = _head->next
-			return true;
-		}
-		else // 현재 기준으로 _head와 oldHead가 다르면
-		{
-			oldHead = _head; // 1) oldHead = _head
-			return false;
-		}*/
-		while (oldHead && _head.compare_exchange_weak(oldHead, oldHead->next) == false); // 2) _head = _head->next
-		
-		if (oldHead == nullptr) // oldHead가 nullptr이면
-		{
-			--_popcount; // popcount 감소
-			return false; // false 리턴
-		}
+			// 참조권 획득 (externalCount를 현 시점 기준 +1 한 애가 이김)
+			IncreaseHeadCount(oldHead);
+			// 최소한 externalCount가 2이어야 함 고로 삭제X
+			Node* ptr = oldHead.ptr;
 
-		value = oldHead->data; // 3) 기존 _head data 추출 / 반환
-		TryDelete(oldHead); // 4) 기존 _head 삭제
+			if (ptr == nullptr)
+				return shared_ptr<T>();
 
-		return true;
-	}
-
-	void TryDelete(Node* oldHead)
-	{
-		// 나 외에 누가 이 함수에 들어와 있는지?
-		if (_popcount == 1) // 나만 들어와 있으면
-		{
-			// 나만 있는게 확실하니, 삭제 예약된 다른 데이터도 삭제하자
-			Node* node = _pendingList.exchange(nullptr); 
-
-			if (--_popcount == 0) // 끼어든 애가 없다면
+			// 소유권 획득 (ptr->next 로 head를 바꿔치기 한 애가 이김)
+			if (_head.compare_exchange_strong(oldHead, ptr->next))
 			{
-				// 삭제 진행
-				DeleteNodes(node);
-			}	
-			else if (node)// 누가 끼어들었으니 다시 데이터 반환
-			{
-				ChainPendingNodeList(node);
+				shared_ptr<T> res;
+				res.swap(ptr->data);
+
+				// 나 말고 또 누가 있는가?
+				const int32 countIncrease = oldHead.externalCount - 2;
+				if (ptr->internalCount.fetch_add(countIncrease) == -countIncrease)
+					delete ptr; 
+
+				return res;
 			}
-
-			// 내꺼 데이터 삭제
-			delete oldHead;
+			else if (ptr->internalCount.fetch_sub(1) == 1)
+			{
+				// 참조권은 얻었으나, 소유권은 실패 -> 뒷수습은 내가 한다.
+				delete ptr;
+			}
 		}
-		else 
+
+		while (oldHead.ptr && _head.compare_exchange_weak(oldHead, oldHead.ptr->next) == false)
+			;
+
+		if (oldHead.ptr == nullptr)
+			return shared_ptr<T>();
+
+		return oldHead.ptr->data;
+	}
+
+private:
+	void IncreaseHeadCount(CountedNodePtr& oldCounter)
+	{
+		while (true)
 		{
-			// 누가 있네? 그럼 지금 삭제하지 않고 삭제 예약만
-			ChainPendingNode(oldHead);
-			--_popcount;
-		}
-	}
+			CountedNodePtr newCounter = oldCounter;
+			newCounter.externalCount++;
 
-	// [first][ ][ ][ ][ ][ ][last] -> [_pendingList][ ][ ][ ][ ]...
-	//   ^                         
-	// [_pendingList]              
-	void ChainPendingNodeList(Node* first, Node* last)
-	{
-		last->next = _pendingList;
-
-		while (_pendingList.compare_exchange_weak(last->next, first) == false)
-		{
-
-		}
-	}
-
-	// 위 함수를 오버로딩한 헬퍼 함수
-	void ChainPendingNodeList(Node* node)
-	{
-		Node* last = node;
-		while (last->next)
-			last = last->next;
-
-		ChainPendingNodeList(node, last);
-	}
-
-	void ChainPendingNode(Node* node)
-	{
-		ChainPendingNodeList(node, node);
-	}
-
-	static void DeleteNodes(Node* node)
-	{
-		// [node] < [next] < [next] < ...
-		//   ^  
-		//  삭제
-		while (node)
-		{
-			Node* next = node->next;
-			delete node;
-			node = next;
+			if (_head.compare_exchange_strong(oldCounter, newCounter))
+			{
+				oldCounter.externalCount = newCounter.externalCount;
+				break;
+			}
 		}
 	}
 
 private:
-	// [ ][ ][ ][ ][ ][ ][ ]
-	//  ^  
-	// [_head]
-	atomic<Node*> _head;
-
-	atomic<uint32> _popcount = 0;
-	atomic<Node*> _pendingList;
+	atomic<CountedNodePtr> _head;
 };
