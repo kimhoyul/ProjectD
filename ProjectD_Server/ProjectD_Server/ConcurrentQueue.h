4f334c26a406c87458c05056c5b661dd0a7fc1e0
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2023-12-02 23:10:17 +0900

    LockFreeQueue (LockFree에 대한 이해 보다는 경합을 CAS를 통해 해결하는과정을 이해함)

--- ProjectD_Server/ProjectD_Server/ConcurrentQueue.h
+++ ProjectD_Server/ProjectD_Server/ConcurrentQueue.h
@@ -42,3 +42,243 @@ private:
 	mutex _mutex;
 	condition_variable _condVar;
 };
+
+//template<typename T>
+//class LockFreeQueue
+//{
+//	struct Node
+//	{
+//		shared_ptr<T> data;
+//		Node* next = nullptr;
+//	};
+//
+//public:
+//	LockFreeQueue() : _head(new Node), _tail(_head) 
+//	{
+//
+//	}
+//
+//	LockFreeQueue(const LockFreeQueue&) = delete;
+//	LockFreeQueue& operator=(const LockFreeQueue&) = delete;
+//
+//	void Push(const T& value)
+//	{
+//		shared_ptr<T> newData = make_shared<T>(value);
+//
+//		Node* dummy = new Node; // dummy node 생성
+//
+//		Node* oldTail = _tail;
+//		oldTail->data.swap(newData); // oldTail의 data와 newData의 data를 swap
+//		oldTail->next = dummy; // oldTail의 next를 dummy로 설정
+//		
+//		_tail = dummy;
+//	}
+//
+//	shared_ptr<T> TryePop()
+//	{
+//		Node* oldHead = PopHead(); // PopHead를 호출하여 oldHead에 저장
+//		if (oldHead == nullptr) // oldHead가 nullptr이면
+//			return shared_ptr<T>(); // 빈 값 반환
+//
+//		shared_ptr<T> res(oldHead->data); // oldHead의 data를 res에 저장
+//		delete oldHead; // oldHead 삭제
+//
+//		return res; // res 반환
+//	}
+//
+//private:
+//	Node* PopHead()
+//	{
+//		Node* oldHead = _head; // oldHead에 _head를 저장
+//		if (oldHead == _tail) // _head와 _tail이 같으면 dummy를 가르키고 있으므로 널 반환
+//			return nullptr;
+//
+//		_head = oldHead->next; // _head를 oldHead의 next로 설정
+//		return oldHead; // oldHead 반환
+//
+//	}
+//
+//private:
+//	// [data][data][data][data]
+//	//   ^					^
+//	// [head]			 [tail]
+//	Node* _head;
+//	Node* _tail;
+//};
+
+template<typename T>
+class LockFreeQueue
+{
+	struct Node;
+
+	struct CountedNodePtr
+	{
+		int32 externalCount = 0; // 참조권
+		Node* ptr = nullptr;
+	};
+
+	struct NodeCounter
+	{
+		uint32 internalCount : 30; // 참조권 반환 관련
+		uint32 externalCountRemaining : 2; // Push, Pop 다중 참조권 관련
+	};
+
+	struct Node
+	{
+		Node()
+		{
+			NodeCounter newCounter;
+			newCounter.internalCount = 0;
+			newCounter.externalCountRemaining = 2;
+			count.store(newCounter);
+
+			next.ptr = nullptr;
+			next.externalCount = 0;
+		}
+
+		void ReleaseRef()
+		{
+			NodeCounter oldCounter = count.load();
+
+			while (true)
+			{
+				NodeCounter newCounter = oldCounter;
+				newCounter.internalCount--;
+
+				if (count.compare_exchange_strong(oldCounter, newCounter))
+				{
+					if (newCounter.internalCount == 0 && newCounter.externalCountRemaining == 0)
+						delete this;
+
+					break;
+				}
+			}
+		}
+
+		atomic<T*> data;
+		atomic<NodeCounter> count;
+		CountedNodePtr next;
+	};
+
+public:
+	LockFreeQueue()
+	{
+		CountedNodePtr node;
+		node.ptr = new Node;
+		node.externalCount = 1;
+
+		_head.store(node);
+		_tail.store(node);
+	}
+
+	LockFreeQueue(const LockFreeQueue&) = delete;
+	LockFreeQueue& operator=(const LockFreeQueue&) = delete;
+
+	void Push(const T& value)
+	{
+		unique_ptr<T> newData = make_unique<T>(value);
+
+		CountedNodePtr dummy;
+		dummy.ptr = new Node;
+		dummy.externalCount = 1;
+
+		CountedNodePtr oldTail = _tail.load(); // ptr = nullptr
+
+		while (true)
+		{
+			// 참조권 획득
+			IncreaseExternalCount(_tail, oldTail);
+
+			// 소유권 획득
+			T* oldData = nullptr; 
+			if (oldTail.ptr->data.compare_exchange_strong(oldData, newData.get())) // oldData가 nullptr이면 newData.get()으로 설정
+			{
+				oldTail.ptr->next = dummy;
+				oldTail = _tail.exchange(dummy); // dummy를 _tail에 저장
+				
+				FreeExternalCounter(oldTail);
+
+				newData.release();
+				break;
+			}
+
+			// 소유권 경쟁 패배
+			oldTail.ptr->ReleaseRef();
+		}
+	}
+
+	shared_ptr<T> TryPop()
+	{
+		CountedNodePtr oldHead = _head.load();
+
+		while (true)
+		{
+			// 참조권 획득
+			IncreaseExternalCount(_head, oldHead);
+			
+			Node* ptr = oldHead.ptr;
+			if (ptr == _tail.load().ptr)
+			{
+				ptr->ReleaseRef();
+				return shared_ptr<T>();
+			}
+
+			// 소유권 획득
+			if (_head.compare_exchange_strong(oldHead, ptr->next))
+			{
+				T* res = ptr->data.exchange(nullptr);
+				FreeExternalCounter(oldHead);
+				return shared_ptr<T>(res);
+			}
+
+			// 소유권 경쟁 패배
+			ptr->ReleaseRef();
+		}
+	}
+
+private:
+	static void IncreaseExternalCount(atomic<CountedNodePtr>& counter, CountedNodePtr& oldCounter)
+	{
+		while (true)
+		{
+			CountedNodePtr newCounter = oldCounter;
+			newCounter.externalCount++;
+
+			if (counter.compare_exchange_strong(oldCounter, newCounter))
+			{
+				oldCounter.externalCount = newCounter.externalCount;
+				break;
+			}
+		}
+	}
+
+	static void FreeExternalCounter(CountedNodePtr& oldNodePtr)
+	{
+		Node* ptr = oldNodePtr.ptr;
+		const int32 countIncrease = oldNodePtr.externalCount - 2;
+
+		NodeCounter oldCounter = ptr->count.load();
+		
+		while (true)
+		{
+			NodeCounter newCounter = oldCounter;
+			newCounter.externalCountRemaining--;
+			newCounter.internalCount += countIncrease;
+
+			if (ptr->count.compare_exchange_strong(oldCounter, newCounter))
+			{
+				if (newCounter.internalCount == 0 && newCounter.externalCountRemaining == 0)
+					delete ptr;
+
+				break;
+			}
+		}
+	}
+
+private:
+	// [data][data][data][data]
+	//   ^					^
+	// [head]			 [tail]
+	atomic<CountedNodePtr> _head;
+	atomic<CountedNodePtr> _tail;
+};
--- ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
+++ ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
@@ -9,7 +9,7 @@
 #include "ConcurrentQueue.h"
 #include "ConcureentStack.h"
 
-LockQueue<int32> q;
+LockFreeQueue<int32> q;
 LockFreeStack<int32> s;
 
 void Push()
@@ -17,7 +17,7 @@ void Push()
 	while (true)
 	{
 		int32 value = rand() % 100;
-		s.Push(value);
+		q.Push(value);
 
 		this_thread::sleep_for(10ms);
 	}
@@ -27,7 +27,7 @@ void Pop()
 {
 	while (true)
 	{
-		auto data = s.TryPop();
+		auto data = q.TryPop();
 		if (data != nullptr)
 			cout << (*data)<< endl;
 	}
