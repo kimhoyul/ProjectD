9c990fd3536e92e668578cf0ad57e30eae38adbc
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2023-12-11 23:28:06 +0900

    StructList 공부

---
+++ ProjectD_Server/ProjectD_Server/LockFreeStack.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/*-------------------------------------------
+				   1Â÷ ½Ãµµ
+
+-------------------------------------------*/
+struct SListEntry
+{
+	SListEntry* next;
+};
+
+
+struct SListHeader
+{
+	SListEntry* next = nullptr;
+};
+
+void InitializeHead(SListHeader* header);
+void PushEntrySList(SListHeader* header, SListEntry* entry);
+SListEntry* PopEntrySList(SListHeader* header);
