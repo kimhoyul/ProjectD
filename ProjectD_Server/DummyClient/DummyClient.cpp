d7f83fc658946c218fb87cc97669b22227854ab4
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2023-12-18 00:38:32 +0900

    UDP 기초

--- ProjectD_Server/DummyClient/DummyClient.cpp
+++ ProjectD_Server/DummyClient/DummyClient.cpp
@@ -6,69 +6,75 @@
 #include <ws2tcpip.h> // for inet_ntop
 #pragma comment(lib, "ws2_32.lib") // Winsock Library
 
+void HandleError(const char* cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	cout << cause << " : " << errCode << endl;
+}
+
 int main()
 {
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-	{
 		return 0;
-		cout << "WSAStartup failed" << endl;
-	}
 	
-	SOCKET clientSocket = ::socket(AF_INET,	SOCK_STREAM, 0);
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "socket failed : " << errCode << endl;
+		HandleError("Socket failed");
 		return 0;
 	}
 
 	SOCKADDR_IN serverAddr; 
 	::memset(&serverAddr, 0, sizeof(serverAddr)); 
 	serverAddr.sin_family = AF_INET; 
-	inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); 
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 	serverAddr.sin_port = ::htons(7777); 
-
-	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "connect failed : " << errCode << endl;
-		return 0;
-	}
-	 
+		 
+	// Connected UDP
+	::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+		 
 	// ------------------------------------
-	// 연결 성공!
-
-	cout << "connect success" << endl;
 
 	while (true)
 	{
 		char sendBuffer[100] = "Hello World!";
 
-		for (int32 i = 0; i < 10; ++i)
+		// Unconnected UDP
+		/*int32 errCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0,
+			(SOCKADDR*)&serverAddr, sizeof(serverAddr));*/
+
+		// Connected UDP
+		int32 errCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+
+		if (errCode == SOCKET_ERROR)
 		{
-			int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
-			if (resultCode == SOCKET_ERROR)
-			{
-				int32 errCode = ::WSAGetLastError();
-				cout << "send failed : " << errCode << endl;
-				return 0;
-			}
+			HandleError("SendTo failed");
+			return 0;
 		}
 
 		cout << "send success Data Len : " << sizeof(sendBuffer) << endl;
 
-		/*char recvBuffer[1000];
+		SOCKADDR_IN recvAddr;
+		::memset(&recvAddr, 0, sizeof(recvAddr));
+		int32 addrLen = sizeof(recvAddr);
+
+		char recvBuffer[1000];
+
+		// Unconnected UDP
+		/*int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0,
+			(SOCKADDR*)&recvAddr, &addrLen);*/
+
+		// Connected UDP
 		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
 		if (recvLen <= 0)
 		{
-			int32 errCode = ::WSAGetLastError();
-			cout << "recv failed : " << errCode << endl;
+			HandleError("Recvfrom failed");
 			return 0;
 		}
 
-		cout << "recv success Data : " << recvBuffer << endl;
-		cout << "size : " << recvLen << endl;*/
+		cout << "Recv Data : " << recvBuffer << endl;
+		cout << "Recv Data Len : " << recvLen << endl;
 
 		this_thread::sleep_for(1s);
 	}
--- ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
+++ ProjectD_Server/ProjectD_Server/ProjectD_Server.cpp
@@ -13,87 +13,69 @@
 #include <ws2tcpip.h> // for inet_ntop
 #pragma comment(lib, "ws2_32.lib") // Winsock Library
 
+void HandleError(const char* cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	cout << cause << " : " << errCode << endl;
+}
+
 int main()
 {	
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-	{
 		return 0;
-		cout << "WSAStartup failed" << endl;
-	}
 
-	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
-	if (listenSocket == INVALID_SOCKET)
+	SOCKET serverSocket = ::socket(AF_INET,	SOCK_DGRAM, 0);
+	if (serverSocket == INVALID_SOCKET)
 	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "socket failed : " << errCode << endl;
+		HandleError("Socket failed");
 		return 0;
 	}
 
-	SOCKADDR_IN serverAddr; 
+	SOCKADDR_IN serverAddr;
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
 	serverAddr.sin_port = ::htons(7777);
 
-	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "bind failed : " << errCode << endl;
+		HandleError("Bind failed");
 		return 0;
 	}
 
-	if (::listen(listenSocket, 10) == SOCKET_ERROR)
-	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "listen failed : " << errCode << endl;
-		return 0;
-	}
-	
 	while (true)
 	{
 		SOCKADDR_IN clientAddr;
 		::memset(&clientAddr, 0, sizeof(clientAddr));
 		int32 addrLen = sizeof(clientAddr);
 
-		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-		if (clientSocket == INVALID_SOCKET)
+		this_thread::sleep_for(1s);
+
+		char recvBuffer[1000];
+
+		int32 recvLen = ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0, 
+			(SOCKADDR*)&clientAddr, &addrLen);
+
+		if (recvLen <= 0)
 		{
-			int32 errCode = ::WSAGetLastError();
-			cout << "accept failed : " << errCode << endl;
+			HandleError("Recvfrom failed");
 			return 0;
 		}
 
-		char ipAddress[16];
-		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
-		cout << "client connected! IP : " << ipAddress << endl;
+		cout << "Recv Data : " << recvBuffer << endl;
+		cout << "Recv Data Len : " << recvLen << endl;
 
-		while (true)
+		int32 errCode = ::sendto(serverSocket, recvBuffer, recvLen, 0,
+			(SOCKADDR*)&clientAddr, sizeof(clientAddr));
+
+		if (errCode == SOCKET_ERROR)
 		{
-			char recvBuffer[1000];
-
-			this_thread::sleep_for(1s);
-
-			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-			if (recvLen <= 0)
-			{
-				int32 errCode = ::WSAGetLastError();
-				cout << "recv failed : " << errCode << endl;
-				return 0;
-			}
-
-			cout << "recv success Data : " << recvBuffer << endl;
-			cout << "size : " << recvLen << endl;
-
-			/*int32 resultCode = ::send(clientSocket, recvBuffer, recvLen, 0);
-			if (resultCode == SOCKET_ERROR)
-			{
-				int32 errCode = ::WSAGetLastError();
-				cout << "send failed : " << errCode << endl;
-				return 0;
-			}*/
+			HandleError("SendTo failed");
+			return 0;
 		}
 
+		cout << "Send Data Len : " << recvLen << endl;
 	}
 
 	::WSACleanup();
