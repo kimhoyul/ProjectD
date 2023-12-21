d35b0142b77a1481e8d45fbaa9a76de772afdab1
Younghoon Kim <96504568+kimhoyul@users.noreply.github.com>
2023-12-21 17:03:35 +0900

    IOCP 코어 제작중

--- ProjectD_Server/ServerCore/IocpEvent.cpp
+++ ProjectD_Server/ServerCore/IocpEvent.cpp
@@ -1,2 +1,22 @@
 #include "pch.h"
 #include "IocpEvent.h"
+
+/*-------------------------------------------
+				  IocpEvent
+
+	OVERLAPPED 사용하는 객체들의 부모 클래스
+-------------------------------------------*/
+
+IocpEvent::IocpEvent(EventType eventType)
+{
+	Init();
+}
+
+void IocpEvent::Init()
+{
+	OVERLAPPED::hEvent = 0;
+	OVERLAPPED::Internal = 0;
+	OVERLAPPED::InternalHigh = 0;
+	OVERLAPPED::Offset = 0;
+	OVERLAPPED::OffsetHigh = 0;
+}
