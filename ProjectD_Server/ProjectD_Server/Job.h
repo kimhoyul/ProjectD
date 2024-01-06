a2028cb2ed6ec911f532f35eda5550e298eb7c80
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2024-01-06 14:27:11 +0900

    JobQueue C++11 apply 함수 형식으로 구현

--- ProjectD_Server/ProjectD_Server/Job.h
+++ ProjectD_Server/ProjectD_Server/Job.h
@@ -1,6 +1,43 @@
 #pragma once
 
+// C++11 apply 구현
+template<int... Remains>
+struct seq
+{};
 
+template<int N, int... Remains>
+struct gen_seq : gen_seq<N - 1, N - 1, Remains...>
+{};
+
+template<int... Remains>
+struct gen_seq<0, Remains...> : seq<Remains...>
+{};
+
+template<typename Ret, typename... Args>
+void xapply(Ret(*func)(Args...), std::tuple<Args...>& tup)
+{
+	xapply_helper(func, gen_seq<sizeof...(Args)>(), tup);
+}
+
+template<typename F, typename... Args, int... ls>
+void xapply_helper(F func, seq<ls...>, std::tuple<Args...>& tup)
+{
+	(func)(std::get<ls>(tup)...);
+}
+
+template<typename T, typename Ret, typename... Args>
+void xapply(T* obj, Ret(T::*func)(Args...), std::tuple<Args...>& tup)
+{
+	xapply_helper(obj, func, gen_seq<sizeof...(Args)>(), tup);
+}
+
+template<typename T, typename F, typename... Args, int...ls>
+void xapply_helper(T* obj, F func, seq<ls...>, std::tuple<Args...>& tup)
+{
+	(obj->*func)(std::get<ls>(tup)...);
+}
+
+// 함수자(Functor) : 함수처럼 동작하는 객체
 class IJob
 {
 public:
@@ -8,19 +45,49 @@ public:
 
 };
 
-class HealJob : public IJob
+template<typename Ret, typename... Args>
+class FuncJob : public IJob
 {
-public:
-	virtual void Execute() override
-	{
-		// _target을 찾아서
-		// _target->AddHP(_healValue);
-		cout << _target << "한테 힐" << _healValue << " 만큼 줌" << endl;
-	}
+	using FuncType = Ret(*)(Args...);
 
 public:
-	uint64 _target = 0;
-	uint32 _healValue = 0;	
+	FuncJob(FuncType func, Args... args) : _func(func), _tuple(args...)
+	{
+	
+	}
+
+	virtual void Execute() override
+	{
+		// std::apply(_func, _tuple); // C++17
+		xapply(_func, _tuple); // C++11
+	}
+
+private:
+	FuncType _func;
+	std::tuple<Args...> _tuple;
+};
+
+template<typename T, typename Ret, typename... Args>
+class MemberJob : public IJob
+{
+	using FuncType = Ret(T::*)(Args...);
+
+public:
+	MemberJob(T* obj, FuncType func, Args... args) : _obj(obj), _func(func), _tuple(args...)
+	{
+
+	}
+
+	virtual void Execute() override
+	{
+		// std::apply(_func, _tuple); // C++17
+		xapply(_obj, _func, _tuple); // C++11
+	}
+
+private:
+	T* _obj;
+	FuncType _func;
+	std::tuple<Args...> _tuple;
 };
 
 using JobRef = shared_ptr<IJob>;
